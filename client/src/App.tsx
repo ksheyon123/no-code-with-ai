233b6ec3cd422db40e55fdded0d61a8d2aaa6658
Kanseohyeon <kshyeon123@gmail.com>
2025-02-26 16:50:43 +0900

    정리

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,7 @@
 import React from "react";
-import "./styles/App.css";
 
 const App: React.FC = () => {
-  return (
-    <div className="app">
-      <header className="app-header">
-        <h1>React + Webpack + TypeScript</h1>
-        <p>
-          절대 경로 예시: <code>@/components</code>를 사용하여 컴포넌트를
-          임포트할 수 있습니다.
-        </p>
-      </header>
-    </div>
-  );
+  return <div className="app"></div>;
 };
 
 export default App;
